149b93d6b0b9291ed983ba3abd3b776db29647a9
sergeykondr <sergeykondr@gmail.com>
2020-05-31 23:57:00 +0300

    add FutureBuilder, ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:restaurants_app/inst.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static final String restaurantsUrl = 'https://www.avtobanket.ru/restorany-dlya-svadby/?get_map_insts=true';
+  final Future<Inst> instList = InstApi().fetchInst();
+
   @override
   Widget build(BuildContext context) {
+    Future<Inst> instList = InstApi().fetchInst();
+
     return MaterialApp(
       title: 'Restaurants app',
       theme: ThemeData(primaryColor: Colors.white), //
       home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          title: Text('Рестораны'),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            instCard(),
-            instCard(),
-            instCard(),
-          ],
-        ),
-      ),
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title: Text('Рестораны'),
+            centerTitle: true,
+          ),
+          body: FutureBuilder<Inst>(
+            future: instList,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data.insts.length ,  
+                  itemBuilder: (context, index){
+                    print(snapshot.data.insts[index].instMainPhoto);
+                    return Card(
+                    child: ListTile(
+                      title: Text('${snapshot.data.insts[index].name}'),
+                      subtitle: Text('${snapshot.data.insts[index].address}'),
+                      leading: Image.network('https://www.avtobanket.ru' + 
+                          '${snapshot.data.insts[index].instMainPhoto}'),
+                      isThreeLine: true,
+                    ),
+                  );
+                  });
+              } else if (snapshot.hasError) {
+                return Text('Error');
+              }
+              return Text('Загрузка...');
+            },
+          )
+
+          // Column(
+          //   children: <Widget>[
+          //     instCard(),
+          //     instCard(),
+          //     instCard(),
+          //     //Page2()
+          //   ],
+          // ),
+          ),
     );
   }
 }
@@ -37,7 +71,7 @@ Widget instCard() {
       children: <Widget>[
         Row(
           children: <Widget>[
-            Image.network( //wrapper expanded
+            Image.network(//wrapper expanded
                 'https://www.avtobanket.ru/upload/image_gallery/1637/80x80_crop_51fff5999065ae69ec8da2dba1fb166d.jpg')
           ],
         ),
@@ -78,4 +112,20 @@ Widget instCard() {
       ],
     ),
   );
+}
+
+
+class InstApi {
+  Future<Inst> fetchInst() async {
+    final responce = await http
+        .post(MyApp.restaurantsUrl, headers: {'fromapp': 'flutter/app'});
+
+    print(responce.body);
+    if (responce.statusCode != 200) {
+      throw Exception('Error responce: ${responce.reasonPhrase}');
+    }
+
+    return Inst.fromJson(json.decode(responce.body));
+    //return responce.body;
+  }
 }
