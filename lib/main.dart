0fb432fc19a0c0c522e2a433ee8ae182d9b4bf23
sergeykondr <sergeykondr@gmail.com>
2020-06-02 00:12:19 +0300

    add spinkit, StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'dart:convert';
 
 import 'package:restaurants_app/inst.dart';
@@ -9,61 +10,89 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  static final String restaurantsUrl = 'https://www.avtobanket.ru/restorany-dlya-svadby/?get_map_insts=true';
-  final Future<Inst> instList = InstApi().fetchInst();
+  static final String restaurantsUrl =
+      'https://www.avtobanket.ru/restorany-dlya-svadby/?get_map_insts=true';
 
   @override
   Widget build(BuildContext context) {
-    Future<Inst> instList = InstApi().fetchInst();
-
     return MaterialApp(
       title: 'Restaurants app',
       theme: ThemeData(primaryColor: Colors.white), //
       home: Scaffold(
-          backgroundColor: Colors.white,
-          appBar: AppBar(
-            title: Text('Рестораны'),
-            centerTitle: true,
-          ),
-          body: FutureBuilder<Inst>(
-            future: instList,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data.insts.length ,  
-                  itemBuilder: (context, index){
-                    print(snapshot.data.insts[index].instMainPhoto);
-                    return Card(
-                    child: ListTile(
-                      title: Text('${snapshot.data.insts[index].name}'),
-                      subtitle: Text('${snapshot.data.insts[index].address}'),
-                      leading: Image.network('https://www.avtobanket.ru' + 
-                          '${snapshot.data.insts[index].instMainPhoto}'),
-                      isThreeLine: true,
-                    ),
-                  );
-                  });
-              } else if (snapshot.hasError) {
-                return Text('Error');
-              }
-              return Text('Загрузка...');
-            },
-          )
-
-          // Column(
-          //   children: <Widget>[
-          //     instCard(),
-          //     instCard(),
-          //     instCard(),
-          //     //Page2()
-          //   ],
-          // ),
-          ),
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text('Рестораны'),
+          centerTitle: true,
+        ),
+        body: InstList(),
+      ),
     );
   }
 }
 
-Widget instCard() {
+class InstList extends StatefulWidget {
+  @override
+  _InstListState createState() => _InstListState();
+}
+
+class _InstListState extends State<InstList> {
+  @override
+  Widget build(BuildContext context) {
+    final Future<Inst> instList = InstApi().fetchInst();
+
+    return FutureBuilder<Inst>(
+      future: instList,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView.builder(
+              itemCount: snapshot.data.insts.length,
+              itemBuilder: (context, index) {
+                if (index == 0) {
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 3),
+                        child: Text(
+                          'Рестораны для свадьбы - найдено ${snapshot.data.insts.length}',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      instCard(snapshot, index)
+                    ],
+                  );
+                }
+                return instCard(snapshot, index);
+              });
+        } else if (snapshot.hasError) {
+          return Text('Error');
+        }
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SpinKitWave(
+                color: Colors.black87,
+                size: 60,
+                type: SpinKitWaveType.start,
+                itemCount: 6,
+                duration: Duration(milliseconds: 1500),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 30, top: 30),
+                child: Text(
+                  'Идет загрузка списка ресторанов...',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+Widget instCard(AsyncSnapshot<Inst> snapshot, index) {
   return Container(
     padding: EdgeInsets.all(10),
     child: Column(
@@ -71,35 +100,31 @@ Widget instCard() {
       children: <Widget>[
         Row(
           children: <Widget>[
-            Image.network(//wrapper expanded
-                'https://www.avtobanket.ru/upload/image_gallery/1637/80x80_crop_51fff5999065ae69ec8da2dba1fb166d.jpg')
+            Expanded(
+              child: Image.network(//wrapper expanded
+                  'https://www.avtobanket.ru/upload/image_gallery/1069/510x340_crop_b633d5bbd26aeaed3214b3a0a4246fe9.jpg'),
+            )
           ],
         ),
         Container(
-          //padding: EdgeInsets.fromLTRB(6, 2, 4, 2),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                  'Амбар The Farm',
-                  style: TextStyle(fontSize: 25),
-                ),
-              ),
-              Text(
-                'от 2000 р',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
-        Container(
-          //padding: EdgeInsets.fromLTRB(4, 2, 4, 2),
+          padding: EdgeInsets.only(bottom: 5),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
-                '3 зала на 300, 100 и 70 мест, летняя терраса на 50 мест',
+                '${snapshot.data.insts[index].name}',
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                'от ${snapshot.data.insts[index].minBanquetPrice} р./чел.',
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                '${snapshot.data.insts[index].address}',
+                style: TextStyle(fontSize: 15),
+              ),
+              Text(
+                '${snapshot.data.insts[index].capacity}',
                 style: TextStyle(fontSize: 15),
               ),
               Text(
@@ -114,18 +139,14 @@ Widget instCard() {
   );
 }
 
-
 class InstApi {
   Future<Inst> fetchInst() async {
+    await Future.delayed(const Duration(seconds: 2));
     final responce = await http
         .post(MyApp.restaurantsUrl, headers: {'fromapp': 'flutter/app'});
-
-    print(responce.body);
     if (responce.statusCode != 200) {
       throw Exception('Error responce: ${responce.reasonPhrase}');
     }
-
     return Inst.fromJson(json.decode(responce.body));
-    //return responce.body;
   }
 }
